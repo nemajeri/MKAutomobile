0723878b2650341de055f48f0d391ed00d5b7e8b
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-05-09 14:02:22 +0200

    Responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react' 
 import './Navbar.css'
+import { useState } from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 
 function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <div className='mka__navbar'>
-      <div className='mka__navbar-links'>
          <div className='mka__navbar-links'>
          <div className='mka__navbar-links_container'>
            <p><a href='/'>HOME</a></p>
@@ -14,11 +17,30 @@ function Navbar() {
            <p><a href='/fahrzeugankauf'>FAHRZEUGANKAUF</a></p>
            <p><a href='/galerie'>GALERIE</a></p>
            <p><a href='/kontakt'>KONTAKT</a></p>
-
+        </div>
+        <div className='mka__navbar-menu'>
+          {toggleMenu
+          ? <AiOutlineClose color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <GiHamburgerMenu color="#fff" size={27} onClick={() => setToggleMenu(true)} /> 
+          }
+          {
+            toggleMenu && (
+              <div className='mka__navbar-menu_container scale scale-up-center'>
+                <div className='mka__navbar-menu_container-links'>
+                <p><a href='/'>HOME</a></p>
+           <p><a href='/fahrzeugangebote'>FAHRZEUGANGEBOTE</a></p>
+           <p><a href='/finanzierung'>FINANZIERUNG</a></p>
+           <p><a href='/fahrzeugankauf'>FAHRZEUGANKAUF</a></p>
+           <p><a href='/galerie'>GALERIE</a></p>
+           <p><a href='/kontakt'>KONTAKT</a></p>
+                </div>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
-  </div>
+
   );
 }
 
