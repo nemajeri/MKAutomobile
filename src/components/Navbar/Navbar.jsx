2cd0ea0b907a034e2d249451da531633c812ed7b
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-05-22 23:14:20 +0200

    Small changes to mav responsivnes and Search icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,25 +19,7 @@ function Navbar() {
            <p><a href='/fahrzeugankauf'>FAHRZEUGANKAUF</a></p>
            <p><a href='/galerie'>GALERIE</a></p>
            <p><a href='/kontakt'>KONTAKT</a></p>
-           
-        </div>
-        <div className='dropdown'>
-          {toggleSearch
-          ? <AiOutlineClose color="#fff" size={14} onClick={() => setSearchWindow(false)} />
-          : <FaSearch color="#fff" onmouseover="#cf2e2e" size={14} onClick={() => setSearchWindow(true)} />
-          }
-          {
-            toggleSearch && (
-              <form>
-                 <label>
-                   <input type="text" name="name" value="Suche... " />
-                      <i>
-                       <FaSearch />
-                      </i>
-                  </label>
-               </form>
-            )
-          }
+           <p><i><FaSearch /></i></p>
         </div>
         <div className='mka__navbar-menu'>
           {toggleMenu
