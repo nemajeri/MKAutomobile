e648647ec235698cf07c72043f8175dfb6cc24dd
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-05-24 23:21:14 +0200

    Added Navlink to highlight current page and tried to make nav menu responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css'
 import { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Hamburger from 'hamburger-react'
+import { NavLink } from 'react-router-dom'
 
 
 function Navbar() {
@@ -12,13 +13,14 @@ function Navbar() {
     <div className='mka__navbar'>
          <div className='mka__navbar-links'>
          <div className='mka__navbar-links_container'>
-           <p><a href='/'>HOME</a></p>
-           <p><a href='/fahrzeugangebote'>FAHRZEUGANGEBOTE</a></p>
-           <p><a href='/finanzierung'>FINANZIERUNG</a></p>
-           <p><a href='/fahrzeugankauf'>FAHRZEUGANKAUF</a></p>
-           <p><a href='/galerie'>GALERIE</a></p>
-           <p><a href='/kontakt'>KONTAKT</a></p>
-           <p><i><FaSearch onClick={() => setSearchWindow(!toggleSearch)} /></i></p>
+           <p id='/'><NavLink to='/'>HOME</NavLink></p>
+           <p id='/fahrzeugangebote'><NavLink to='/fahrzeugangebote'>FAHRZEUGANGEBOTE</NavLink></p>
+           <p id='/finanzierung'><NavLink to='/finanzierung'>FINANZIERUNG</NavLink></p>
+           <p id='/fahrzeugankauf'><NavLink to='/fahrzeugankauf'>FAHRZEUGANKAUF</NavLink></p>
+           <p id='/galerie'><NavLink to='/galerie'>GALERIE</NavLink></p>
+           <p id='/kontakt'><NavLink to='/kontakt'>KONTAKT</NavLink></p>
+           <div className='mka__search_column'>
+        <p><i><FaSearch onClick={() => setSearchWindow(!toggleSearch)} /></i></p>
            {
             toggleSearch ?
               <div className='mka__search-div'>
@@ -26,24 +28,25 @@ function Navbar() {
               </div>
            : null
           }
-        </div>
+           </div>
+          </div>
         <div className='mka__navbar-menu'>
         {
             toggleMenu ?
-              <div className='mka__navbar-menu_container scale scale-up-center'>
-                <div className='mka__navbar-menu_container-links'>
-                <p><a href='/'>HOME</a></p>
-           <p><a href='/fahrzeugangebote'>FAHRZEUGANGEBOTE</a></p>
-           <p><a href='/finanzierung'>FINANZIERUNG</a></p>
-           <p><a href='/fahrzeugankauf'>FAHRZEUGANKAUF</a></p>
-           <p><a href='/galerie'>GALERIE</a></p>
-           <p><a href='/kontakt'>KONTAKT</a></p>
+            <div className='mka__navbar-menu_container'>
+              <div className='mka__navbar-menu_container-links'>
+                <p><NavLink to='/'>HOME</NavLink></p>
+           <p><NavLink to='/fahrzeugangebote'>FAHRZEUGANGEBOTE</NavLink></p>
+           <p><NavLink to='/finanzierung'>FINANZIERUNG</NavLink></p>
+           <p><NavLink to='/fahrzeugankauf'>FAHRZEUGANKAUF</NavLink></p>
+           <p><NavLink to='/galerie'>GALERIE</NavLink></p>
+           <p><NavLink to='/kontakt'>KONTAKT</NavLink></p>
                 </div>
-              </div>
+                </div>
            : null
           }
-          <Hamburger color="#fff" direction="right" duration="0.1" size={27} onClick={() => setToggleMenu(!toggleMenu)} /> 
-        </div>
+             <Hamburger color="#fff" direction="right" duration="0.1" size={27} onClick={() => setToggleMenu(!toggleMenu)} /> 
+          </div>
       </div>
     </div>
 
