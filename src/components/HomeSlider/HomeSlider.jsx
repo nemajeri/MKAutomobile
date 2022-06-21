3a1a8a6f64660bbbf39b0623e73a5347dc4b6913
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-06-21 16:02:11 +0200

    Working on Responsivness of a Home page slider

--- src/components/HomeSlider/HomeSlider.jsx
+++ src/components/HomeSlider/HomeSlider.jsx
@@ -9,7 +9,7 @@ function SampleNextArrow(props) {
   const { onClick } = props;
   return (
     <div
-      className="slick-next1"
+      className="slick-next-home-slider"
       onClick={onClick}
     />
   );
@@ -19,36 +19,80 @@ function SamplePrevArrow(props) {
   const { onClick } = props;
   return (
     <div
-      className="slick-prev1"
+      className="slick-prev-home-slider"
       onClick={onClick}
     />
   );
 }
 
 class HomeSlider extends Component {
+  state = {
+    display: true,
+    width: 280
+  }
   render () {
   const settings = {
+    className: "slider variable-width",
     dots: false,
     infinite: true,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
-    speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
   };
   
 
   return (
-    <div className="mka__main-gallery-div">
+<div className="mka__home-page-slider">
       <Slider {...settings}>
+        <div>
        <Card/>
+       </div>
+       <div>
        <Card/>
+       </div>
+       <div>
        <Card/>
+       </div>
+       <div>
        <Card/>
+       </div>
+       <div>
        <Card/>
+       </div>
+       <div>
        <Card/>
+       </div>
       </Slider>
-    </div>
+      </div>
+
   );
 }
 }
