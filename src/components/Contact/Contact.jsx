cae64d184e6e7cd908a976102e415209990bb65d
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-06-22 09:29:41 +0200

    Working on Contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,64 @@ import './Contact.css';
 
 function Contact() {
   return (
-    <div>
+    <div className='mka__container-contact'>
+      <div className='mka__content-contact'>
+      <div className='mka__contact-infos-container'>
+        <div className='mka__contact-infos-card'>
+          <div className='mka__contact-infos-circle'></div>
+          <h5>ADRESSE</h5>
+          <div className='mka__contact-infos-divider'></div>
+          <p className='mka__contact-infos-description'>
+          Im Trenkenschuh 15-17
+          2100 Korneuburg
+          </p>
+        </div>
+        <div className='mka__contact-infos-card'>
+          <div className='mka__contact-infos-circle'></div>
+          <h5>TELEFON</h5>
+          <div className='mka__contact-infos-divider'></div>
+          <p className='mka__contact-infos-description'>
+          +43 680 3149332
+          +43 2262 20413
+          </p>
+        </div>
+        <div className='mka__contact-infos-card'>
+          <div className='mka__contact-infos-circle'></div>
+          <h5>EMAIL</h5>
+          <div className='mka__contact-infos-divider'></div>
+          <p className='mka__contact-infos-description'>
+          office@mkautomobile.at
+          </p>
+        </div>
+        <div className='mka__forms-purchase'>
+        <div className='mka__form-design'>
+        <form>
+        <label>
+          <input type="text" value="VORNAME"/>
+          <input type="text" value="TEL"/>
+          </label>
+        </form>
+        </div>
+        <div className='mka__form-design'>
+        <form>
+        <label>
+          <input type="text" value="NACHNAME"/>
+          <input type="text" value="FIRMA"/>
+          </label>
+        </form>
+        </div>
+        <div className='mka__form-design'>
+        <form>
+        <label>
+          <input type="text" value="E-MAIL"/>
+          <input type="text" value="IHRE NACHRICHT"/>
+          </label>
+        </form>
+        </div>
+        </div>
+        <button type='submit' className='mka__form-button'>SENDEN</button>
+      </div>
+    </div>
     </div>
   );
 }
--- src/pages/Kontakt/Kontakt.jsx
+++ src/pages/Kontakt/Kontakt.jsx
@@ -1,11 +1,31 @@
 import './Kontakt.css';
+import {Header, Navbar, Footer, Jumbotrone, Logo, Contact} from '../../components'
 
 function Kontakt() {
   return (
     <div>
-      <h1>Kontakt</h1>
-    </div>
-  );
-}
+    <div id='contact-img-id' className='contact-img-div'>
+       <Header/>
+        <Navbar />
+        <div className='mka__logo-img'>
+          <div className='mka__logo-size'>
+          <Logo/>
+          </div>
+          </div>
+          <p id="mka__contact-description">Probefahrt und Besichtigung sind ausschließlich nach Terminvereinbarung möglich</p>
+        <div className='mka__title-wrapper'>
+        <div className='mka__wrapper-contact'>
+        <div className='mka__container'>
+        <h1 id='mka__contact-title'>KONTAKT</h1>
+        <Contact/>
+        </div>
+        </div>
+        </div>
+        </div>
+  <Jumbotrone/>
+    <Footer/>
+  </div>
+    );
+  }
 
 export default Kontakt;
