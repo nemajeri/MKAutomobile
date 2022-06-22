79c405af210c6c103941526fa458746c2cde39eb
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-06-22 18:17:35 +0200

    Responsivness of Contact page left

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -63,16 +63,30 @@ function Contact() {
         </div>
         <button type='submit' className='mka__form-button'>SENDEN</button>
         <div className='mka__google-api-contact'>
-        <iframe title='Google maps' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2651.6948709379444!2d16.3221874!3d48.34717!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d0e3bfb300989%3A0x729d0e5fe5bbceee!2sIm%20Trenkenschuh%2015-17%2C%202100%20Korneuburg%2C%20%C3%96sterreich!5e0!3m2!1sde!2sba!4v1628134320122!5m2!1sde!2sba" width={300} height={150}></iframe>
-          <div className='mka__openning-times-contact'>
-          <h5>ÖFFNUNGSZEITEN</h5>
-          <p>Montag 10:00h - 18:00h</p>
-          <p>Dienstag 10:00h - 18:00h</p>
-          <p>Mittwoch 10:00h - 18:00h</p>
-          <p>Donnerstag 10:00h - 18:00h</p>
-          <p>Freitag 10:00h - 18:00h</p>
-          <p>Samstag 10:00h - 12:00h</p>
-          <p>Sonntag Geschlossen</p>
+        <iframe title='Google maps' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2651.6948709379444!2d16.3221874!3d48.34717!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d0e3bfb300989%3A0x729d0e5fe5bbceee!2sIm%20Trenkenschuh%2015-17%2C%202100%20Korneuburg%2C%20%C3%96sterreich!5e0!3m2!1sde!2sba!4v1628134320122!5m2!1sde!2sba" width={750} height={450}></iframe>
+          <div className='mka__opening-times-contact'>
+          <h6>ÖFFNUNGSZEITEN</h6>
+          <div className='mka__opening-times-divider'></div>
+          <div className='mka__opening-times-text'>
+          <div>
+          <p><strong>Montag</strong></p> 
+          <p><strong>Dienstag</strong></p>
+          <p><strong>Mittwoch</strong></p>
+          <p><strong>Donnerstag </strong></p>
+          <p><strong>Freitag </strong></p>
+          <p><strong>Samstag</strong></p>
+          <p><strong>Sonntag</strong></p> 
+          </div>
+          <div>
+          <p>10:00h - 18:00h</p>
+          <p>10:00h - 18:00h</p>
+          <p>10:00h - 18:00h</p>
+          <p>10:00h - 18:00h</p>
+          <p>10:00h - 18:00h</p>
+          <p>10:00h - 12:00h</p>
+          <p id='mka__text-red'>Geschlossen</p>
+          </div>
+          </div>
           </div>
         </div>
       </div>
