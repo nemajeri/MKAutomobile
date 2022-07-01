11ab10c975c146067fcbee53fe8ab15dc51a21b3
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-07-01 20:41:16 +0200

    Working on Search filter

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-
+import { FaSearch } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import './Search.css'
 
 class Search extends Component {
     state = {
         text: '',
     }
 
-    onChange = (e) => this.setState({text: e.target.value})
+    onChange = e => this.setState({text: e.target.value}) 
+
+    handleChange = () => {
+        this.props.carSearch(this.state.text);
+        this.setState({text: ''})
+    }
 
     render() {
 
     return (
-    <div>
+    <div className='mka__left-alignment'>
         <h6>FAHRZEUGSUCHE</h6>
         <div className='mka__search-divider'></div>
         <br />
@@ -21,8 +28,12 @@ class Search extends Component {
                   type="text"
                   value={this.state.text}
                   placeholder="Suche..."
-                  onChange={this.onChange}
+                  onChange={"this.onChange; this.handleChange"}
                 />
+                <IconContext.Provider value={{ className: "fa-search-car-offers" }}>
+                  {" "}
+                  <FaSearch />
+                </IconContext.Provider>
          </form>
               </div>
         )
