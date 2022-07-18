3cfde4b27536643eb1b523c2d776d35b1f446f1b
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-07-18 23:03:29 +0200

    Pretvorio u funkcionalnu komponentu

--- src/components/CarOffers/CarAlignment.jsx
+++ src/components/CarOffers/CarAlignment.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import AlignItem1 from './AlignItem1'
 import { AlignItem2 } from './AlignItem2'
 import { AlignItem3 } from './AlignItem3'
 import { AlignItem4 } from './AlignItem4'
 import './CarAlignment.css'
 
- class CarAlignment extends Component {
-  render() {
+ const CarAlignment = () => {
     return (
       <div className='mka__div-icon1'>
             <span className='mka__span-icon1'>
@@ -32,7 +31,6 @@ import './CarAlignment.css'
 
       </div>
     )
-  }
 }
 
 export default CarAlignment
