0f857b17dd70c295f2267187550aa893cfc908d0
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-07-19 13:15:58 +0200

    Napravio novu klasu za divider jer nisam mogao dodati klasu  na postojecu

--- src/components/HomePageCarList/HomePageCarList.jsx
+++ src/components/HomePageCarList/HomePageCarList.jsx
@@ -9,7 +9,7 @@ const HomePageCarList = () => {
       <div className="mka__container-home-list">
         <div className="mka__content">
           <h2 className="mka__content-home-title"> FAHRZEUGANGEBOTE </h2>
-          <div className="mka__section-divider second-divider"></div>
+          <div className="mka__section-divider-second-divider"></div>
           <HomeSlider/>
           <div className="mka__section-divider"></div>
           <div className="mka__shortcuts-home-list">
