327f4d5dfc36743c68c1c737c812e5f4db244c00
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-07-28 15:27:30 +0200

    Ostala samo funkcionalnost inputa nove vrijednosti

--- src/components/CarOffers/Search.jsx
+++ src/components/CarOffers/Search.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
 import Select from 'react-select';
+import './Search.css'
 
 
 const Search = ({ value, carsList, changeInput}) => {
 
   const carsOptions = carsList.map((car) => {
     return {
-      value: car.make,
-      label: car.make
+      value: car.title,
+      label: car.title
     }
   })
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      color: 'grey',
+      padding: 20,
+    }),
+    valueContainer: (provided, state) => ({
+      ...provided,
+      backgroundColor: 'transparent',
+      border: state.isFocused ? '1px solid red' : '1px solid white',
+      padding: '5px',
+    }),
+    control: () => ({
+      width: 200,
+    })
+  }
     return (
       <>
   <Select
   cacheOptions
   value={value}
+  placeholder='Suche...'
   options={carsOptions}
+  styles={customStyles}
   defaultOptions
   onInputChange={changeInput}
+  components={{ DropdownIndicator:() => null, IndicatorSeparator:() => null }}
+  formatOptionLabel={car => (
+    <div className="country-option">
+      <img src={car.link} alt="car-label"/>
+      <span>{car.label}</span>
+    </div>
+  )}
     />
       </>
     );
