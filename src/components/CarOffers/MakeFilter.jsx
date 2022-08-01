bf60f30ef7bfc4c6a322d1cc0c3ed3335a26de2d
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-08-01 20:01:45 +0200

    Odvojio filter polje

---
+++ src/components/CarOffers/MakeFilter.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Select from 'react-select';
+
+const colourStyles = {
+  control: (styles) => ({ ...styles, backgroundColor: "transparent" }),
+  option: (styles, { isDisabled }) => {
+    return {
+      ...styles,
+      backgroundColor: isDisabled ? "transparent" : "grey" ,
+      color: "#FFF",
+      cursor: isDisabled ? "not-allowed" : "default"
+    };
+  }
+};
+
+
+const MakeFilter = ({carsList,handleSelectMake}) => {
+  const distinctBy = (arr, f) => {
+    return arr.filter((a, i) => arr.findIndex((b) => f(a) === f(b)) === i);
+  }
+  
+  const options = carsList
+    .map((car) => {
+      return {
+        value: car.make,
+        label: car.make
+      };
+    })
+   
+   const distinctOptions = distinctBy(options, car => car.value)
+
+    return (
+            <>
+<Select  options={distinctOptions}  onChange={handleSelectMake} styles={colourStyles} isSearchable={false}/>
+            </>
+        )
+    }
+
+export default MakeFilter
