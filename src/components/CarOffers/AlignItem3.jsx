f6fee39910a3bbcb183ee559e65ae65f3d2ba534
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-08-05 23:24:09 +0200

    Prepravio sintaksnu gresku i instalirao swipper

--- src/components/CarOffers/AlignItem3.jsx
+++ src/components/CarOffers/AlignItem3.jsx
@@ -5,7 +5,7 @@ export const AlignItem3 = () => {
     <div><svg xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 10.91 8.77"><defs>
     </defs><g id="Layer_2" data-name="Layer 2">
-    <g id="Layer_1-2" data-name="Layer 1"><rect class="cls-1" width="1.74" height="8.77"/>
+    <g id="Layer_1-2" data-name="Layer 1"><rect className="cls-1" width="1.74" height="8.77"/>
     <rect className="cls-1" x="2.42" y="4.88" width="8.49" height="1.47"/><rect className="cls-1" x="2.42" y="2.43" width="8.49" height="1.47"/>
     <rect className="cls-1" x="2.42" y="7.32" width="8.49" height="1.45"/><rect className="cls-1" x="2.42" width="8.49" height="1.47"/></g></g>
     </svg>
