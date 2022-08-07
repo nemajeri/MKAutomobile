d3dd241dc8e2c2528508657cc61d4bcc6916863c
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-08-07 21:40:11 +0200

    Sredjujem css za Kontakt stranicu

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -51,14 +51,15 @@ const Contact = () => {
             </div>
           </div>
           <ContactForm />
-          <div className="mka__google-api-contact">
+          <div className="mka__google-api-contact">    
+          <div className="mka__google-api-container">
             <iframe
               title="Google maps"
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2651.6948709379444!2d16.3221874!3d48.34717!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d0e3bfb300989%3A0x729d0e5fe5bbceee!2sIm%20Trenkenschuh%2015-17%2C%202100%20Korneuburg%2C%20%C3%96sterreich!5e0!3m2!1sde!2sba!4v1628134320122!5m2!1sde!2sba"
-              width={750}
+              width={'100%'}
               height={450}
-              style={{width: "100%"}}
             ></iframe>
+            </div>
             <div className="mka__opening-times-contact">
               <h6>ÖFFNUNGSZEITEN</h6>
               <div className="mka__opening-times-divider"></div>
