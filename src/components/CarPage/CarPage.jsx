2f6d6af0d64779d6b2e4975699e581932e31c655
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-08-13 19:43:46 +0200

    Radim na fahrzeugdaten

--- src/components/CarPage/CarPage.jsx
+++ src/components/CarPage/CarPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CarPage.css'
-import { FaExchangeAlt } from 'react-icons/fa'
+import CarEquipment from './CarEquipment';
+import CarDescription from './CarDescription'
+import { FaExchangeAlt, FaSlidersH } from 'react-icons/fa'
+import MainProductPageSlider from '../MainProductPageSlider/MainProductPageSlider'
+import HomeSlider from '../HomeSlider/HomeSlider'
 
 const CarPage = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  const handleClick = () => {
+    setIsActive(false)
+  }
+
   return (
     <div className="mka__wrapper-error">
     <div className="mka__container">
@@ -33,28 +43,36 @@ const CarPage = () => {
         </div>
         <div className='mka__description-section__carpage'>
           <div className='mka__gallery-carpage'>
-            Ovdje ce biti galerija
+          <MainProductPageSlider/>
           </div>
           <div className='mka__description-carpage'>
             <h6>BESCHREIBUNG</h6>
             <div className='mka__section-divider short-carpage'></div>
-            <ul>
-              <li><p>Erstzulassung</p><strong>01/2006</strong></li>
-              <li><p>Marke</p><strong>BMW</strong></li>
-              <li><p>Modell</p><strong>3er-Reihe</strong></li>
-              <li><p>Fahrzeugtype</p><strong>Limousine</strong></li>
-              <li><p>Zustand</p><strong>Gebrauchtwagen</strong></li>
-              <li><p>Kilometer</p><strong>154.000</strong></li>
-              <li><p>Getriebeart</p><strong>Automatik</strong></li>
-              <li><p>Antrieb</p><strong>Hinterrad</strong></li>
-              <li><p>Hubraum</p><strong>2.497 cm³</strong></li>
-              <li><p>Treibstoff</p><strong>Benzin</strong></li>
-              <li><p>Leistung</p><strong>160kw /218 PS</strong></li>
-              <li><p>Farbe</p><strong>Silber</strong></li>
+            <ul className='mka__list-carpage'>
+              <li><div className='mka__list-content__carpage'><p>Erstzulassung</p><div className='mka__strong-alignment__carpage'><strong>01/2006</strong></div></div></li>
+              <li><div className='mka__list-content__carpage'><p>Marke</p><strong>BMW</strong></div></li>
+              <li><div className='mka__list-content__carpage'><p>Modell</p><strong>3er-Reihe</strong></div></li>
+              <li><div className='mka__list-content__carpage'><p>Fahrzeugtype</p><strong>Limousine</strong></div></li>
+              <li><div className='mka__list-content__carpage'><p>Zustand</p><strong>Gebrauchtwagen</strong></div></li>
+              <li><div className='mka__list-content__carpage'><p>Kilometer</p><strong>154.000</strong></div></li>
+              <li><div className='mka__list-content__carpage'><p>Getriebeart</p><div className='mka__strong-alignment__carpage'><strong>Automatik</strong></div></div></li>
+              <li><div className='mka__list-content__carpage'><p>Antrieb</p><strong>Hinterrad</strong></div></li>
+              <li><div className='mka__list-content__carpage'><p>Hubraum</p><strong>2.497 cm³</strong></div></li>
+              <li><div className='mka__list-content__carpage'><p>Treibstoff</p><strong>Benzin</strong></div></li>
+              <li><div className='mka__list-content__carpage'><p>Leistung</p><strong>160kw /218 PS</strong></div></li>
+              <li><div className='mka__list-content__carpage'><p>Farbe</p><strong>Silber</strong></div></li>
             </ul>
           </div>
         </div>
+              <div className='mka__description-tabs__carpage'>
+              <ul>
+                <li className='mka__description-tab--underline__carpage' onClick={(prevState) => setIsActive(prevState)}><i><FaSlidersH/></i>AUSSTATUNG</li>
+                <li className='mka__description-tab--underline__carpage' onClick={handleClick}><i><FaSlidersH/></i>ZUSATZINFORMATION</li>
+              </ul>
+              {isActive ? <CarEquipment/> : <CarDescription/>}
+              </div>
       </div>
+              <HomeSlider/>
     </div>
   </div>
   )
