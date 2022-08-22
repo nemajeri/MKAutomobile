7b09b6b6fba955227a0fe5f508d300ec81408401
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-08-22 22:37:16 +0200

    Radim na product stranici

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Datenschutz, Fahrzeugangebote, Fahrzeugankauf, Finanzierung, Galerie, Home, Impressum, Kontakt, Fehler404, Fahrzeugdaten } from './pages'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
--- src/components/CarPage/CarPage.jsx
+++ src/components/CarPage/CarPage.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import './CarPage.css'
+import { useParams } from 'react-router-dom'
+import './CarPage.css';
 import CarEquipment from './CarEquipment';
 import CarDescription from './CarDescription'
-import { FaExchangeAlt, FaSlidersH } from 'react-icons/fa'
-import MainProductPageSlider from '../MainProductPageSlider/MainProductPageSlider'
-import HomeSlider from '../HomeSlider/HomeSlider'
+import { FaExchangeAlt, FaSlidersH } from 'react-icons/fa';
+import MainProductPageSlider from '../MainProductPageSlider/MainProductPageSlider';
+import HomeSlider from '../HomeSlider/HomeSlider';
 
 const CarPage = () => {
+  const params = useParams();
+  const [data, setData] = useState([])
   const [isActive, setIsActive] = useState(true);
   const [cars, setCars] = useState([]);
 
@@ -16,26 +19,34 @@ const CarPage = () => {
 
   const url = 'http://finity.pro/clients/mkautomobile/cars/all';
 
-  useEffect(() => {
+  const fetchSimilarCars = () => {
     fetch(url)
     .then(response => response.json())
     .then(cars => {
-    console.log(cars)  
     const currentCars = cars.slice(-5);
     setCars(currentCars)
+    setData(cars)
   });
+  }
+
+  const car = data.find(params => params == data.id)
+
+  console.log(car)
+
+  useEffect(() => {
+    fetchSimilarCars()
   }, []);
 
   return (
-    <div className="mka__wrapper-error">
+    <div className="mka__wrapper-carpage">
     <div className="mka__container">
-      <div className='mka__content-error'>
+      <div className='mka__content-carpage'>
         <div className='mka__title-price-carpage'>
-          <h4>
+          <h3>
             BMW 3ER-REIHE 325 I XDRIVE (E90) ** ALLRAD - AUTOMATIK** <br/>
             LIMOUSINE
-          </h4>
-          <h5>8.490€</h5>
+          </h3>
+          <h4>8.490€</h4>
         </div>
         <div className='mka__email-filed-carpage'>
             <a className='mka__comparison-carpage'>
@@ -62,25 +73,25 @@ const CarPage = () => {
             <h6>BESCHREIBUNG</h6>
             <div className='mka__section-divider short-carpage'></div>
             <ul className='mka__list-carpage'>
-              <li><div className='mka__list-content__carpage'><p>Erstzulassung</p><div className='mka__strong-alignment__carpage'><strong>01/2006</strong></div></div></li>
-              <li><div className='mka__list-content__carpage'><p>Marke</p><strong>BMW</strong></div></li>
-              <li><div className='mka__list-content__carpage'><p>Modell</p><strong>3er-Reihe</strong></div></li>
-              <li><div className='mka__list-content__carpage'><p>Fahrzeugtype</p><strong>Limousine</strong></div></li>
-              <li><div className='mka__list-content__carpage'><p>Zustand</p><strong>Gebrauchtwagen</strong></div></li>
-              <li><div className='mka__list-content__carpage'><p>Kilometer</p><strong>154.000</strong></div></li>
-              <li><div className='mka__list-content__carpage'><p>Getriebeart</p><div className='mka__strong-alignment__carpage'><strong>Automatik</strong></div></div></li>
-              <li><div className='mka__list-content__carpage'><p>Antrieb</p><strong>Hinterrad</strong></div></li>
-              <li><div className='mka__list-content__carpage'><p>Hubraum</p><strong>2.497 cm³</strong></div></li>
-              <li><div className='mka__list-content__carpage'><p>Treibstoff</p><strong>Benzin</strong></div></li>
-              <li><div className='mka__list-content__carpage'><p>Leistung</p><strong>160kw /218 PS</strong></div></li>
-              <li><div className='mka__list-content__carpage'><p>Farbe</p><strong>Silber</strong></div></li>
+              <li className='mka__car-attributes'><span>Erstzulassung</span><strong>01/2006</strong></li>
+              <li className='mka__car-attributes'><span>Marke</span><strong>BMW</strong></li>
+              <li className='mka__car-attributes'><span>Modell</span><strong>3er-Reihe</strong></li>
+              <li className='mka__car-attributes'><span>Fahrzeugtype</span><strong>Limousine</strong></li>
+              <li className='mka__car-attributes'><span>Zustand</span><strong>Gebrauchtwagen</strong></li>
+              <li className='mka__car-attributes'><span>Kilometer</span><strong>154.000</strong></li>
+              <li className='mka__car-attributes'><span>Getriebeart</span><strong>Automatik</strong></li>
+              <li className='mka__car-attributes'><span>Antrieb</span><strong>Hinterrad</strong></li>
+              <li className='mka__car-attributes'><span>Hubraum</span><strong>2.497 cm³</strong></li>
+              <li className='mka__car-attributes'><span>Treibstoff</span><strong>Benzin</strong></li>
+              <li className='mka__car-attributes'><span>Leistung</span><strong>160kw /218 PS</strong></li>
+              <li className='mka__car-attributes'><span>Farbe</span><strong>Silber</strong></li>
             </ul>
           </div>
         </div>
               <div className='mka__description-tabs__carpage'>
               <ul>
-                <li className='mka__description-tab--underline__carpage' onClick={(prevState) => setIsActive(prevState)}><i><FaSlidersH/></i>AUSSTATUNG</li>
-                <li className='mka__description-tab--underline__carpage' onClick={handleClick}><i><FaSlidersH/></i>ZUSATZINFORMATION</li>
+                <li className={`mka__description-tab__carpage ${isActive ? "selected" : ""}`} onClick={(prevState) => setIsActive(prevState)}><i><FaSlidersH/></i>AUSSTATUNG</li>
+                <li className={`mka__description-tab__carpage ${!isActive ? "selected" : ""}`}  onClick={handleClick}><i><FaSlidersH/></i>ZUSATZINFORMATION</li>
               </ul>
               {isActive ? <CarEquipment/> : <CarDescription/>}
               </div>
